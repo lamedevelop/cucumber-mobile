9e546c1a3df49711e96db686814370c6611b7e83
zoglam <anzoor1998@gmail.com>
2021-07-18 18:52:52 +0300

    [CU-6] added test drawer

---
+++ lib/widgets/custom_scaffold.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import 'package:cucumber_mobile/widgets/bars/bars.dart';
+import 'package:cucumber_mobile/config/palette.dart' as palette;
+
+class CustomScaffold extends StatelessWidget {
+  final bool hasTopBar;
+  final bool hasBottomBar;
+  final bool hasDrawer;
+  final Widget body;
+
+  const CustomScaffold({
+    Key? key,
+    required this.body,
+    this.hasTopBar = true,
+    this.hasBottomBar = true,
+    this.hasDrawer = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(children: [
+          body,
+          hasTopBar ? _topBarBuilder(context) : Container(child: null),
+          hasBottomBar ? _bottomBarBuilder(context) : Container(child: null),
+        ]),
+      ),
+      drawer: hasDrawer ? _drawerBuilder(context) : null,
+    );
+  }
+
+  Widget _topBarBuilder(BuildContext context) {
+    return Positioned(
+      top: 0,
+      child: MainTopBar(),
+      width: MediaQuery.of(context).size.width,
+    );
+  }
+
+  Widget _bottomBarBuilder(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      child: BottomBarCustom(),
+      width: MediaQuery.of(context).size.width,
+    );
+  }
+
+  Widget _drawerBuilder(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        canvasColor: Colors.transparent,
+      ),
+      child: Drawer(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: palette.Black.PRIMARY,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ),
+          ),
+          child: ListView(
+            children: [
+              Text(
+                'Test row',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
